912aa23bf79581391e9ea8ec3d91e8619be6aa3d
Andric Ishami <ishamand7@gmail.com>
2026-09-24 11:56:26 +0300

    Implement APK download route using Backblaze B2

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import tempfile
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from gradio_client import Client, handle_file
+import boto3
 
 app = Flask(__name__)
 
@@ -10,20 +11,40 @@ app = Flask(__name__)
 HF_SPACE = os.environ["HF_SPACE"]
 HF_TOKEN = os.environ["HF_TOKEN"]
 
+# B2 credentials loaded as environment variables
+B2_KEY_ID = os.environ["B2_KEY_ID"]
+B2_APPLICATION_KEY = os.environ["B2_APPLICATION_KEY"]
+
+# B2 configuration
+B2_BUCKET = "VScreen"
+B2_ENDPOINT = "s3.eu-central-003.backblazeb2.com"
+B2_REGION = "eu-central-003"
+B2_APK_KEY = "downloads/myapp.apk"
+
+# S3 client
+s3 = boto3.client(
+    "s3",
+    endpoint_url=B2_ENDPOINT,
+    aws_access_key_id=B2_KEY_ID,
+    aws_secret_access_key=B2_APPLICATION_KEY,
+    region_name=B2_REGION,
+)
+
+# HF client
 client = Client(
     HF_SPACE,
     token=HF_TOKEN,
     verbose=False
 )
 
-
+# health route
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({
         "success": True
     })
 
-
+# prediction route pointing to hugging face
 @app.route("/predict", methods=["POST"])
 def predict():
     file = request.files.get("audio")
@@ -78,6 +99,19 @@ def predict():
     finally:
         if temp_path and os.path.exists(temp_path):
             os.remove(temp_path)
+
+# download apk route directing to B2
+@app.route("/download", methods=["GET"])
+def download():
+    url = s3.generate_presigned_url(
+        "get_object",
+        Params={
+            "Bucket": B2_BUCKET,
+            "Key": B2_APK_KEY,
+        },
+        ExpiresIn=300,
+    )
+    return redirect(url)
 
 
 if __name__ == "__main__":
